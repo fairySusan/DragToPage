a82f356f12c09d5a79497ca825d05dad3ce02160
taorui <taorui@chinaexpressair.com>
2021-03-12 17:17:30 +0800

    fix bug

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -16,16 +16,15 @@ class Editor extends React.Component<Props> {
 
   render () {
     const { canvasStyle, componentData } = this.props
-    console.log(componentData)
     return (
       <div className="editor" id="editor" style={{width: changeStyleWithScale(canvasStyle.width) + 'px', height: changeStyleWithScale(canvasStyle.height) + 'px'}}>
         <GridLine/>
         {
           componentData.length > 0 && componentData.map((item, i) => {
-            const childCom: any = componentFactory(item.component, item)
+            const ChildCom = componentFactory(item.component, item)
             return (
               <Shape key={i} config={item}>
-                <childCom></childCom>
+                <ChildCom></ChildCom>
               </Shape>
             )
           })
